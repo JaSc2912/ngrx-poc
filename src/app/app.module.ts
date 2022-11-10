68dfe38a8ca4710b5d628f78bcf321c227a3bff4
JaSc2912 <janniklas.schreder@gmx.de>
2022-11-10 10:11:29 +0100

    1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CounterComponent } from './counter/counter.component';
+import { CounterButtonsComponent } from './counter-buttons/counter-buttons.component';
+import { CounterOutputComponent } from './counter-output/counter-output.component';
+import { StoreModule } from '@ngrx/store';
+import { reducer } from './store/counter.reducer';
+import { COUNTER_STATE_NAME } from './store/counter.selector';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CounterComponent,
+    CounterButtonsComponent,
+    CounterOutputComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, StoreModule.forFeature(COUNTER_STATE_NAME, reducer)],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
